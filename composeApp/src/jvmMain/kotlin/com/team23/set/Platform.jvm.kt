7de52a21f3e84dd254f51d63b4d320230d31833d
Hsb511 <hugo-selle@hotmail.fr>
2025-10-22 16:06:47 +0200

    [DEL] Remove useless code

--- composeApp/src/jvmMain/kotlin/com/team23/set/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.team23.set
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
