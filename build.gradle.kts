08109e157d8f048fc835b5ea0dbff256055d1ae9
Hugo Selle <hugo.selle@quickline.net>
2025-10-22 19:13:49 +0200

    [INI] :domain module

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ plugins {
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
     alias(libs.plugins.androidLint) apply false
+    alias(libs.plugins.jetbrainsKotlinJvm) apply false
 }
