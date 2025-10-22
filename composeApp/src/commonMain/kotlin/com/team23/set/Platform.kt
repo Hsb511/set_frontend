cb8c20e79f58c4e8f477c9ad0ed21bb932d726d2
Hugo Selle <hugo.selle@quickline.net>
2025-10-22 16:06:47 +0200

    [DEL] Remove useless code

--- composeApp/src/commonMain/kotlin/com/team23/set/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.team23.set
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/wasmJsMain/kotlin/com/team23/set/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package com.team23.set
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
