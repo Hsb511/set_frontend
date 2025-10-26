97c7a5b333f1380bf2b4e97e7be8781a8160bd0a
Hsb511 <hugo-selle@hotmail.fr>
2025-10-26 16:36:52 +0100

    [FIX] :domain source sets

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,44 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
 }
 
 kotlin {
     jvm()
+
+    val xcfName = "uiKit"
+
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    jvm()
+
+    js {
+        browser()
+        binaries.executable()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
 
     sourceSets {
         commonTest.dependencies {
