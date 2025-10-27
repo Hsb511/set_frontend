a4018543035373d8c73da8c6c2bbee6fb71fcae4
Hugo Selle <hugo.selle@quickline.net>
2025-10-27 20:56:50 +0100

    [ADD] Side effect when the set is invalid + show it in a snackbar

---
+++ ui/src/commonMain/kotlin/com/team23/ui/snackbar/SetSnackbarVisuals.kt
@@ -0,0 +1,14 @@
+package com.team23.ui.snackbar
+
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarVisuals
+
+sealed interface SetSnackbarVisuals: SnackbarVisuals {
+
+    data object InvalidSet: SetSnackbarVisuals {
+        override val message: String = "Not a set!"
+        override val actionLabel: String? = null
+        override val withDismissAction: Boolean = false
+        override val duration: SnackbarDuration = SnackbarDuration.Short
+    }
+}
