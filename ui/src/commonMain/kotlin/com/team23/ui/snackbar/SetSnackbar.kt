66cb296274fedba96ee3da3bd323713344068316
Hsb511 <hugo-selle@hotmail.fr>
2025-12-19 22:23:27 +0100

    [UPD] Dismiss snackbar if a new one is emitted and shown

--- ui/src/commonMain/kotlin/com/team23/ui/snackbar/SetSnackbar.kt
+++ ui/src/commonMain/kotlin/com/team23/ui/snackbar/SetSnackbar.kt
@@ -5,26 +5,36 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 @Composable
 fun SetSnackbar(
     modifier: Modifier = Modifier,
 ) {
+    val coroutineScope = rememberCoroutineScope()
     val lifecycleOwner = LocalLifecycleOwner.current
     val snackbarHostState = remember { SnackbarHostState() }
+    val currentJob = remember { mutableStateOf<Job?>(null) }
 
     LaunchedEffect(Unit) {
         lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
             withContext(Dispatchers.Main.immediate) {
                 SnackbarManager.snackbar.collect { snackbarData ->
-                    snackbarHostState.showSnackbar(snackbarData)
+                    snackbarHostState.currentSnackbarData?.dismiss()
+                    currentJob.value?.cancel()
+                    currentJob.value = coroutineScope.launch {
+                        snackbarHostState.showSnackbar(snackbarData)
+                    }
                 }
             }
         }
